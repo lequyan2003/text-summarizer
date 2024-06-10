49014777557521f798d98e77620498395c7ae10c
lequyan2003 <lequyan2003@gmail.com>
2024-06-10 17:52:51 -0400

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from text_summarizer.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline
+)
 from text_summarizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
