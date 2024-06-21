2eedcca726de6d9fdaae65e0070a5ec971c593c4
lequyan2003 <lequyan2003@gmail.com>
2024-06-21 19:40:06 -0400

    prediction and UI added

---
+++ src/text_summarizer/pipeline/prediction.py
@@ -0,0 +1,25 @@
+from transformers import AutoTokenizer, pipeline
+
+from text_summarizer.config.configuration import ConfigurationManager
+
+
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluation_config()
+
+    def predict(self, text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
+
+        pipe = pipeline(
+            "summarization", model=self.config.model_path, tokenizer=tokenizer
+        )
+
+        print("Dialogue:")
+        print(text)
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        print("\nModel Summary:")
+        print(output)
+
+        return output
